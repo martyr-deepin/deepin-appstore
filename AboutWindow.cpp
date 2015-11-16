47a6b62aeae7b122e0b97ca4e5a21b32a206c927
Xinkai <chenxinkai@linuxdeepin.com>
2015-11-16 16:36:21 +0800

    adjust AboutWindow styles
    Change-Id: I3d92b33f71ead5db94d6fd7f802667e5b919dfac

--- AboutWindow.cpp
+++ AboutWindow.cpp
@@ -1,5 +1,6 @@
 #include <QDesktopServices>
 #include <QTextBrowser>
+#include <QHBoxLayout>
 #include "AboutWindow.h"
 
 AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent) {
@@ -8,10 +9,14 @@ AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent) {
     this->setAutoFillBackground(true);
     this->setAttribute(Qt::WA_DeleteOnClose);
     this->setFixedSize(400, 340);
+    this->setStyleSheet("AboutWindow { background: white }");
+
+    auto horizontalLayout = new QHBoxLayout(this);
     this->content = new QTextBrowser(this);
     this->content->setTextInteractionFlags(Qt::LinksAccessibleByMouse |
                                            Qt::LinksAccessibleByKeyboard);
-    this->content->setFixedSize(400, 340);
+    this->content->setFixedSize(350, 340);
+    this->content->setStyleSheet("QTextBrowser { border: 0 }");
 
     // handle anchors
     this->content->setOpenLinks(false);
@@ -21,6 +26,11 @@ AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent) {
             QDesktopServices::openUrl(url);
         }
     });
+
+    horizontalLayout->setSpacing(0);
+    horizontalLayout->setObjectName("horizontalLayout");
+    horizontalLayout->setContentsMargins(25, 0, 25, 0);
+    horizontalLayout->addWidget(this->content);
 }
 
 void AboutWindow::setContent(QString& html) {
