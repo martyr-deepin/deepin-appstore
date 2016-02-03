cf61b996a5529da07c35d2986d11b4a3b629d65a
snyh <snyh@snyh.org>
2016-02-03 10:15:54 +0800

    Add copyright information
    Change-Id: Idc2f3e87dcf521b8d71c4051c045a6dd0debf7de

--- Bridge.cpp
+++ Bridge.cpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/
 #define QT_NO_KEYWORDS
     // for opening .desktop files
     #include <gio/gio.h>
--- WebKit/LocalFileSystemReply.h
+++ WebKit/LocalFileSystemReply.h
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/
 #ifndef SHELL_LOCALFILESYSTEMREPLY_H
 #define SHELL_LOCALFILESYSTEMREPLY_H
 
