e7e92ce886c078c49832d34818fd4b20c3eae636
xushaohua <xushaohua@deepin.com>
2018-04-08 13:31:15 +0800

    resources: Add settings field name
    Change-Id: I1806cf4c5cd5edfaa98d770b0c9ef37b5785a4d7

---
+++ src/resources/settings.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2017 ~ $year Deepin Technology Co., Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DEEPIN_APPSTORE_RESOURCES_SETTINGS_H
+#define DEEPIN_APPSTORE_RESOURCES_SETTINGS_H
+
+namespace dstore {
+
+extern const char kSupportSignIn[];
+
+extern const char kMetadataServer[];
+
+extern const char kOperationServer[];
+
+}  // namespace dstore
+
+#endif  // DEEPIN_APPSTORE_RESOURCES_SETTINGS_H
