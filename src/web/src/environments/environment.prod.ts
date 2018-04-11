ae0b7f52c86b17d09e32ae6903fd987ebbbf3c35
xushaohua <xushaohua@deepin.com>
2018-04-11 10:00:33 +0800

    web: Add server address to environment
    Change-Id: I2f58c7732bbfeac5fb64868acad3b74c1a095d43

--- src/web/src/environments/environment.prod.ts
+++ src/web/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Server address only used in browser mode.
+  metadataServer: 'http://10.0.12.148:8000',
+  operationServer: 'http://10.0.12.148:8100',
 };
--- src/web/src/environments/environment.ts
+++ src/web/src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // Server address only used in browser mode.
+  metadataServer: 'http://10.0.12.148:8000',
+  operationServer: 'http://10.0.12.148:8100',
 };
