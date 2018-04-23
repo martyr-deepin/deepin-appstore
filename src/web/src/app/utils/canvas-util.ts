c1c4e9296f6950aec0d7e959935e75c25ffe6b07
xushaohua <xushaohua@deepin.com>
2018-04-23 14:40:37 +0800

    web: Add CanvasUtil class
    Change-Id: I3733a17cb6b5a18fb48f09db2c8d40e8c89992be

---
+++ src/web/src/app/utils/canvas-util.ts
@@ -0,0 +1,21 @@
+
+export class CanvasUtil {
+
+  /**
+   * Convert the content of an img element into base64-encoded string.
+   * @param {HTMLImageElement} img
+   * @param {string} format Output image format, default is JPEG.
+   * @returns {string}
+   */
+  static getBase64Image(img: HTMLImageElement, format? = 'image/jpg'): string {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+
+    const dataURL = canvas.toDataURL(format);
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+  }
+}
