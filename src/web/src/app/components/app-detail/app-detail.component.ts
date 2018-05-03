206915495cd3fd8e1c17375ea456bd9edcc5332f
myml <wurongjie1@gmail.com>
2018-05-03 11:30:26 +0800

    feat: open app button
    Change-Id: I47649c5b79dc4a2a6bfa4c5bd6cfb14f63df92c8

--- src/web/src/app/components/app-detail/app-detail.component.ts
+++ src/web/src/app/components/app-detail/app-detail.component.ts
@@ -7,6 +7,7 @@ import { App } from '../../dstore/services/app';
 import { AppService } from '../../services/app.service';
 import { BaseService } from '../../dstore/services/base.service';
 import { CanvasUtil } from '../../utils/canvas-util';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-app-detail',
@@ -14,8 +15,16 @@ import { CanvasUtil } from '../../utils/canvas-util';
   styleUrls: ['./app-detail.component.scss'],
 })
 export class AppDetailComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private appService: AppService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private appService: AppService,
+    private storeService: StoreService,
+  ) {}
 
+  open = this.storeService.openApp;
+
+  appStatus = appStatus;
+  status$: Observable<appStatus>;
   metadataServer: string;
   appObs: Observable<App>;
 
@@ -24,26 +33,53 @@ export class AppDetailComponent implements OnInit {
     this.appObs = this.route.paramMap.mergeMap(param => {
       const appName = param.get('appName');
       console.log('app name:', appName);
-      return this.appService
-        .getApp(appName)
-        .do(app => console.log('app info:', app));
+      return this.appService.getApp(appName).do(app => console.log('app info:', app));
     });
+
+    this.status$ = Observable.timer(0, 3000)
+      .mergeMap(() =>
+        this.appObs.mergeMap(app =>
+          this.storeService.appInstalled(app.name).mergeMap(exists => {
+            if (exists) {
+              return Observable.of(appStatus.finish);
+            } else {
+              return this.storeService
+                .getJobList()
+                .mergeMap(jobs =>
+                  Observable.forkJoin(...jobs.map(job => this.storeService.getJobInfo(job))),
+                )
+                .map(jobInfoList => {
+                  if (jobInfoList.map(jobInfo => jobInfo.name).includes(app.name)) {
+                    return appStatus.running;
+                  } else {
+                    return appStatus.ready;
+                  }
+                });
+            }
+          }),
+        ),
+      )
+      .do(status => console.log(status));
+  }
+
+  install(appName: string) {
+    this.storeService.installPackage(appName).subscribe();
   }
 
   screenshotClick(elID: string) {
-    ScrollIntoView(document.getElementById(elID));
-    // chrome version 61 support
-    // (<HTMLElement>event.target).scrollIntoView({
+    // chrome 61 support
+    // const opt: ScrollIntoViewOptions = {
     //   behavior: 'smooth',
     //   inline: 'center',
-    //   block: 'nearest'
-    // });
+    //   block: 'nearest',
+    // };
+    // (<HTMLElement>event.target).scrollIntoView(opt);
+
+    ScrollIntoView(document.getElementById(elID));
   }
 
   previewImage(img: HTMLImageElement) {
-    window['dstore'].channel.objects.imageViewer.openBase64(
-      CanvasUtil.getBase64Image(img),
-    );
+    window['dstore'].channel.objects.imageViewer.openBase64(CanvasUtil.getBase64Image(img));
   }
 
   log(v) {
@@ -51,12 +87,8 @@ export class AppDetailComponent implements OnInit {
   }
 }
 
-interface Desc {
-  Rate: number;
-  Category: number;
-  Version: string;
-  Size: string;
-  UpdateDate: Date;
-  OfficialWebsite: string;
-  Introduction: string;
+enum appStatus {
+  ready,
+  running,
+  finish,
 }
