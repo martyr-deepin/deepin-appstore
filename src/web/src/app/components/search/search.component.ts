bf1a6c9a9bea4da933f2245580aee9241277bdf6
myml <wurongjie1@gmail.com>
2018-05-09 13:31:47 +0800

    feat: wait component
    Change-Id: Iba78e043f015f6541336285963d6cce582f3e6ce

--- src/web/src/app/components/search/search.component.ts
+++ src/web/src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+import { truncate } from 'lodash';
 
 import { App, AppService } from '../../services/app.service';
 import { SortOrder } from '../app-title/app-title.component';
@@ -21,10 +22,11 @@ export class SearchComponent implements OnInit {
   sortBy = SortOrder.Downloads;
   keyword: string;
   apps$: Observable<App[]>;
+
   ngOnInit() {
     this.apps$ = this.route.paramMap.mergeMap(param => {
       this.keyword = param.get('keyword');
-      // angular bug https://github.com/angular/angular/issues/19179
+      // angular 'getAll' bug https://github.com/angular/angular/issues/19179
       const appNameList = param
         .getAll('apps')
         .map(apps => apps.split(','))
@@ -32,6 +34,10 @@ export class SearchComponent implements OnInit {
       return this.appService.list().map(apps => apps.filter(app => appNameList.includes(app.name)));
     });
   }
+  get title(): string {
+    return `"${truncate(this.keyword, { length: 16 })}"`;
+  }
+
   recommend() {
     this.recommendService.openRecommend();
   }
