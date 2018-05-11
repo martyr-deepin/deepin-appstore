4b89b8006a736d340a5201ff25d0d22818195795
myml <wurongjie1@gmail.com>
2018-05-11 18:34:18 +0800

    feat: category icon
    Change-Id: I907f51b77064769176b5e18c84e030a824e5763b

--- src/web/src/app/components/side-nav/side-nav.component.ts
+++ src/web/src/app/components/side-nav/side-nav.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl, SafeStyle } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { memoize } from 'lodash';
 
 import { CategoryService } from '../../services/category.service';
 import { Category } from '../../services/category.service';
 import { BaseService } from '../../dstore/services/base.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-side-nav',
@@ -11,9 +13,16 @@ import { Observable } from 'rxjs';
   styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
-  constructor(private categoryService: CategoryService) {}
+  constructor(private categoryService: CategoryService, private sanitizer: DomSanitizer) {}
   native = BaseService.isNative;
   cs$: Observable<Category[]>;
+
+  getStyle = memoize((id: string) => {
+    return this.sanitizer.bypassSecurityTrustStyle(
+      `--src: url("/assets/category/${id}.svg");
+       --active: url("/assets/category/${id}_active.svg")`,
+    );
+  });
   ngOnInit() {
     this.cs$ = this.categoryService.list();
   }
