c9d666d02d70e4edd51eecc6fc94d8069f63f28e
myml <wurongjie1@gmail.com>
2018-05-21 11:14:24 +0800

    feat: index
    Change-Id: I5a35f6e2c2e50bccbd2ec073c01d5e9b51755cdd

--- src/web/src/app/components/index/index.component.ts
+++ src/web/src/app/components/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { SectionService } from '../../services/section.service';
 import { Section, SectionType } from '../../dstore/services/section';
@@ -6,14 +8,15 @@ import { Section, SectionType } from '../../dstore/services/section';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
   constructor(private sectionService: SectionService) {}
 
   SectionType = SectionType;
   sectionList: Section[] = [];
+  sectionList$: Observable<Section[]>;
   ngOnInit() {
-    this.sectionService.getList().subscribe(ss => (this.sectionList = ss));
+    this.sectionList$ = this.sectionService.getList().pipe(map(ss => ss.filter(s => s.show)));
   }
 }
