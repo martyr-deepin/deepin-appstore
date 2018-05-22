807fa7ef25ebc455028c0e1ac055bf5805ba04c0
myml <wurongjie1@gmail.com>
2018-05-22 15:50:30 +0800

    fix: route change scrollbar go to top
    Change-Id: I34077fe770c21fac14de0948799777d3225c3fbe

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 import { BaseService } from './dstore/services/base.service';
@@ -8,6 +8,7 @@ import { AppService } from './services/app.service';
 import { SearchService, SearchResult } from './services/search.service';
 import { Channel } from './dstore-client.module/utils/channel';
 import { App } from './dstore/services/app';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +16,19 @@ import { App } from './dstore/services/app';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'app';
-
   constructor(
     private router: Router,
     private appService: AppService,
     private searchService: SearchService,
   ) {}
+  @ViewChild('$context') contentRef: ElementRef<HTMLDivElement>;
 
   ngOnInit(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      console.log('router');
+      this.contentRef.nativeElement.scrollTop = 0;
+    });
+
     if (BaseService.isNative) {
       this.appService.listNoVersion().subscribe((apps: App[]) => {
         const appStringList = JSON.stringify(apps);
