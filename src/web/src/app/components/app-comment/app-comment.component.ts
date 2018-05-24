84f57501bad04c299fa387e8024ea387650f1416
myml <wurongjie1@gmail.com>
2018-05-24 15:07:30 +0800

    fix: back to top
    Change-Id: I1ef9ba8b78b221ef27efc8487f3745ba9c296bf0

--- src/web/src/app/components/app-comment/app-comment.component.ts
+++ src/web/src/app/components/app-comment/app-comment.component.ts
@@ -140,7 +140,7 @@ export class AppCommentComponent implements OnInit {
     }
   }
   scrollToTop() {
-    document.querySelector('.app>.context').scrollTop = 0;
+    window.scrollTo(0, 0);
   }
 }
 
