eb9aa2f00d1a2bc787da54dfbbf757b40e1cefe3
myml <wurongjie1@gmail.com>
2018-05-24 18:11:11 +0800

    fix: uninstall
    Change-Id: Ic0689daf2fd5e4301c5aa337c6b5f76143f216e1

--- src/web/src/app/components/uninstall/uninstall.component.ts
+++ src/web/src/app/components/uninstall/uninstall.component.ts
@@ -6,7 +6,11 @@ import { map, flatMap, switchMap } from 'rxjs/operators';
 import * as _ from 'lodash';
 
 import { StoreService } from '../../dstore-client.module/services/store.service';
-import { StoreJobInfo } from '../../dstore-client.module/models/store-job-info';
+import {
+  StoreJobInfo,
+  StoreJobStatus,
+  StoreJobType,
+} from '../../dstore-client.module/models/store-job-info';
 import { AppService, App } from '../../services/app.service';
 import { BaseService } from '../../dstore/services/base.service';
 
@@ -26,13 +30,23 @@ export class UninstallComponent implements OnInit {
     this.uninstallApps$ = timer(0, 1000).pipe(
       flatMap(() => this.appService.list()),
       map(apps => apps.filter(app => app.version.localVersion)),
+      flatMap(apps =>
+        this.storeService.getVersion(apps.map(app => app.name)).pipe(
+          map(versions => {
+            const versionMap = _.keyBy(versions, 'name');
+            return apps.filter(app => versionMap[app.name] && versionMap[app.name].localVersion);
+          }),
+        ),
+      ),
     );
     this.uninstallJobMap$ = timer(0, 1000).pipe(
       flatMap(() => this.storeService.getJobListInfo()),
       map(jobs => {
         return new Map(
           jobs
-            .filter(job => job.type === 'remove' && job.status === 'running')
+            .filter(
+              job => job.type === StoreJobType.uninstall && job.status !== StoreJobStatus.failed,
+            )
             .map(job => [job.name, job] as [string, StoreJobInfo]),
         );
       }),
