a7741e079e7b08ff5f52319b0fdef5ede06211bb
myml <wurongjie1@gmail.com>
2018-05-25 10:40:58 +0800

    fix bugs
    carousel double click next
    http interceptor toast error
    
    Change-Id: Ide96f2332328c07b62fc8620c034c5383c0442ba

--- src/web/src/app/services/http-interceptor.ts
+++ src/web/src/app/services/http-interceptor.ts
@@ -15,11 +15,7 @@ import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class MyHttpInterceptor implements HttpInterceptor {
-  constructor(
-    private materializeService: MaterializeService,
-    private authService: AuthService,
-    private loginService: LoginService,
-  ) {}
+  constructor(private authService: AuthService, private loginService: LoginService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.authService.isLoggedIn) {
       req = req.clone({ setHeaders: { 'Access-Token': this.authService.token } });
@@ -28,10 +24,9 @@ export class MyHttpInterceptor implements HttpInterceptor {
       catchError((err: HttpErrorResponse, caught) => {
         switch (err.status) {
           case 0:
-            this.materializeService.toastError('网络错误，请检查网络');
             break;
           case 401:
-            this.materializeService.toastError('登录失效，请重新登录');
+            // this.materializeService.toastError('登录失效，请重新登录');
             if (this.authService.isLoggedIn) {
               this.authService.logout();
             } else {
@@ -39,10 +34,10 @@ export class MyHttpInterceptor implements HttpInterceptor {
             }
             return of(null);
           case 429:
-            this.materializeService.toastError('访问过于频繁，请稍后重试');
+            // this.materializeService.toastError('访问过于频繁，请稍后重试');
             break;
           case 500:
-            this.materializeService.toastError('服务器错误，请稍后重试');
+            // this.materializeService.toastError('服务器错误，请稍后重试');
             break;
           default:
             throw err.error.error;
