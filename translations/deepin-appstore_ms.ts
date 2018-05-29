bc2a473309d9a03de994bcb0608e26e1f9e9966f
transifex <transifex@linuxdeepin.com>
2018-05-29 02:32:18 +0000

    auto sync po files from transifex
    Change-Id: I6a71769fd2f5a0324d72d0307fdf232d5b4db506

---
+++ translations/deepin-appstore_ms.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ms" version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="77"/>
+        <source>Deepin App Store</source>
+        <translation>Kedai Apl Deepin</translation>
+    </message>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="78"/>
+        <source>Deepin Store is an Appstore with quality and rich applications. Popular recommendation, new updated apps and hot topics are available. It supports one click to download, install, update, uninstall and so on.</source>
+        <translation>Kedai Deepin ialah Kedai Apl yang mengandungi banyak aplikasi yang berkualiti. Saranan terkenal, apl baharu dikemaskini dan topik panas yang tersedia. Ia juga menyokong sekali klik untuk muat turun, pasang, kemaskini, nyahpasang dan seterusnya.</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="115"/>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="164"/>
+        <source>Search &quot;%1&quot; in Deepin Store</source>
+        <translation>Gelintar &quot;%1&quot; dalam Kedai Deepin</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar.cpp" line="96"/>
+        <source>Search</source>
+        <translation>Gelintar</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="53"/>
+        <source>Sign Out</source>
+        <translation>Daftar Keluar</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="55"/>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="79"/>
+        <source>Sign In</source>
+        <translation>Daftar Masuk</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="83"/>
+        <source>Recommend App</source>
+        <translation>Apl Cadangan</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="87"/>
+        <source>Select Region</source>
+        <translation>Pilih Kawasan</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="88"/>
+        <source>China</source>
+        <translation>China</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="90"/>
+        <source>International</source>
+        <translation>Antarabangsa</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="100"/>
+        <source>Clear Cache</source>
+        <translation>Kosongkan Cache</translation>
+    </message>
+</context>
+</TS>
