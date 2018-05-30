fb44f94edba92b242883eef45c7a5f813a900933
myml <wurongjie1@gmail.com>
2018-05-30 18:40:20 +0800

    feat: download count at side nav
    Change-Id: I6b3e3585fda76852bb59d209b933bf009e40ce19

--- src/web/src/app/components/side-nav/side-nav.component.ts
+++ src/web/src/app/components/side-nav/side-nav.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl, SafeStyle } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { tap, flatMap, map } from 'rxjs/operators';
 
 import { memoize } from 'lodash';
 
 import { CategoryService, makeDefaultCategory } from '../../services/category.service';
 import { Category } from '../../services/category.service';
 import { BaseService } from '../../dstore/services/base.service';
+import { StoreService } from '../../dstore-client.module/services/store.service';
 
 @Component({
   selector: 'app-side-nav',
@@ -15,10 +16,16 @@ import { BaseService } from '../../dstore/services/base.service';
   styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
-  constructor(private categoryService: CategoryService, private sanitizer: DomSanitizer) {}
+  constructor(
+    private categoryService: CategoryService,
+    private sanitizer: DomSanitizer,
+    private storeService: StoreService,
+  ) {}
   native = BaseService.isNative;
   default = makeDefaultCategory();
   cs$: Observable<Category[]>;
+  // download count
+  dc$: Observable<number>;
 
   getStyle = memoize((icon: string[]) => {
     return this.sanitizer.bypassSecurityTrustStyle(
@@ -34,5 +41,9 @@ export class SideNavComponent implements OnInit {
   });
   ngOnInit() {
     this.cs$ = this.categoryService.list();
+    this.dc$ = timer(0, 1000).pipe(
+      flatMap(() => this.storeService.getJobList()),
+      map(jobs => jobs.filter(job => job.includes('install')).length),
+    );
   }
 }
