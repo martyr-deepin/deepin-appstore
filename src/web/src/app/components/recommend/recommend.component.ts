057add13c5828da258de4b489171b7d120b9c41e
myml <wurongjie1@gmail.com>
2018-06-25 13:19:01 +0800

    fix: recommend notify
    Change-Id: I27cd7d34f39a24098347e24b3d2cae60f4db90c2

--- src/web/src/app/components/recommend/recommend.component.ts
+++ src/web/src/app/components/recommend/recommend.component.ts
@@ -3,7 +3,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { RecommendService, Recommend } from '../../services/recommend.service';
-import { MaterializeService } from '../../dstore/services/materialize.service';
+import { NotifyService } from '../../services/notify.service';
+import { NotifyType } from '../../services/notify.model';
 
 @Component({
   selector: 'app-recommend',
@@ -11,10 +12,7 @@ import { MaterializeService } from '../../dstore/services/materialize.service';
   styleUrls: ['./recommend.component.scss'],
 })
 export class RecommendComponent implements OnInit {
-  constructor(
-    private recommendService: RecommendService,
-    private materializeService: MaterializeService,
-  ) {}
+  constructor(private recommendService: RecommendService, private notifyService: NotifyService) {}
   @ViewChild('myDialog') dialogRef: { nativeElement: HTMLDialogElement };
   recommend = new Recommend();
   openDialog$: Observable<void>;
@@ -34,8 +32,11 @@ export class RecommendComponent implements OnInit {
       () => {
         this.dialogRef.nativeElement.close();
         this.recommend = new Recommend();
+        this.notifyService.success(NotifyType.Recommend);
       },
-      () => {},
+      () => {
+        this.notifyService.error(NotifyType.Recommend);
+      },
     );
   }
 }
