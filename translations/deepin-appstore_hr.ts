990b91ec2b20c42a191edb2711c4ec65bb02c874
transifex <transifex@linuxdeepin.com>
2018-07-02 01:44:21 +0000

    auto sync po files from transifex
    Change-Id: I7800fb25c88790c73419ed087fbe76d3d9719175

---
+++ translations/deepin-appstore_hr.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="hr" version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="93"/>
+        <source>Deepin App Store</source>
+        <translation>Deepin trgovina aplikacijama</translation>
+    </message>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="94"/>
+        <source>Deepin Store is an Appstore with quality and rich applications. Popular recommendation, new updated apps and hot topics are available. It supports one click to download, install, update, uninstall and so on.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="115"/>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="164"/>
+        <source>Search &quot;%1&quot; in Deepin Store</source>
+        <translation>Traži &quot;%1&quot;u Deepin trgovini</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar.cpp" line="96"/>
+        <source>Search</source>
+        <translation>Traži</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="51"/>
+        <source>Sign Out</source>
+        <translation>Odjava</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="53"/>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="77"/>
+        <source>Sign In</source>
+        <translation>Prijava</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="81"/>
+        <source>Recommend App</source>
+        <translation>Preporučena aplikacija</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="85"/>
+        <source>Select Region</source>
+        <translation>Odaberi regiju</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="86"/>
+        <source>China</source>
+        <translation>Kina</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="89"/>
+        <source>International</source>
+        <translation>Međunarodno</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="106"/>
+        <source>Clear Cache</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="75"/>
+        <source>Undo</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="80"/>
+        <source>Redo</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="86"/>
+        <source>Cut</source>
+        <translation>Izreži</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="91"/>
+        <source>Copy</source>
+        <translation>Kopiraj</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="96"/>
+        <source>Paste</source>
+        <translation>Zalijepi</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="101"/>
+        <source>Delete</source>
+        <translation>Izbriši</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="107"/>
+        <source>Select all</source>
+        <translation>Odaberi sve</translation>
+    </message>
+</context>
+</TS>
