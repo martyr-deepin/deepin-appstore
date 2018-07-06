2379dc3993061b3cfce590e298ec6055d6a2eb32
Xu Shaohua <xushaohua2016@outlook.com>
2018-07-06 09:20:01 +0800

    Update i18n
    Change-Id: I67dded6dd59db9985b87d28f2369cc0ecc09ab11

---
+++ translations/deepin-appstore_sk.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="93"/>
+        <source>Deepin App Store</source>
+        <translation>Deepin App Store</translation>
+    </message>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="94"/>
+        <source>Deepin Store is an Appstore with quality and rich applications. Popular recommendation, new updated apps and hot topics are available. It supports one click to download, install, update, uninstall and so on.</source>
+        <translation>Deepin Obchod je Appstore s kvalitnými a bohatými aplikáciami. Sú k dispozícii obľúbené odporúčania, nové aktualizované aplikácie a horúce témy. Podporuje stiahnutie, inštaláciu, aktualizáciu, odinštalovanie atď. jedným klikom.</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="115"/>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="164"/>
+        <source>Search &quot;%1&quot; in Deepin Store</source>
+        <translation>Vyhľadať &quot;%1&quot; v Deepin Obchode</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar.cpp" line="96"/>
+        <source>Search</source>
+        <translation>Vyhľadávanie</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="51"/>
+        <source>Sign Out</source>
+        <translation>Odhlásiť sa</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="53"/>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="77"/>
+        <source>Sign In</source>
+        <translation>Prihlásiť sa</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="81"/>
+        <source>Recommend App</source>
+        <translation>Odporúčané aplikácie</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="85"/>
+        <source>Select Region</source>
+        <translation>Výber regiónu</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="86"/>
+        <source>China</source>
+        <translation>Čína</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="89"/>
+        <source>International</source>
+        <translation>Medzinárodný</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="106"/>
+        <source>Clear Cache</source>
+        <translation>Vymazať vyrovnávaciu pamäť</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="75"/>
+        <source>Undo</source>
+        <translation>Vrátenie späť</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="80"/>
+        <source>Redo</source>
+        <translation>Vrátiť dopredu</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="86"/>
+        <source>Cut</source>
+        <translation>Vystrihnúť</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="91"/>
+        <source>Copy</source>
+        <translation>Kopírovať</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="96"/>
+        <source>Paste</source>
+        <translation>Prilepiť</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="101"/>
+        <source>Delete</source>
+        <translation>Vymazať</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="107"/>
+        <source>Select all</source>
+        <translation>Vybrať všetko</translation>
+    </message>
+</context>
+</TS>
