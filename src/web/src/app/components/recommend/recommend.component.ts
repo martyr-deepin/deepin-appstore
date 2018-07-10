08f0d8b9e1a8aa8b4671ce944588f8e9553050b1
myml <wurongjie1@gmail.com>
2018-07-10 15:50:12 +0800

    fix: recommend style
    Change-Id: Ib5778f797cfb9ea79d929c335b9364c22cd7d47c

--- src/web/src/app/components/recommend/recommend.component.ts
+++ src/web/src/app/components/recommend/recommend.component.ts
@@ -5,6 +5,7 @@ import { map } from 'rxjs/operators';
 import { RecommendService, Recommend } from '../../services/recommend.service';
 import { NotifyService } from '../../services/notify.service';
 import { NotifyType } from '../../services/notify.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-recommend',
@@ -14,20 +15,40 @@ import { NotifyType } from '../../services/notify.model';
 export class RecommendComponent implements OnInit {
   constructor(private recommendService: RecommendService, private notifyService: NotifyService) {}
   @ViewChild('myDialog') dialogRef: { nativeElement: HTMLDialogElement };
+  @ViewChild('myForm') form: NgForm;
   recommend = new Recommend();
   openDialog$: Observable<void>;
+  error: { name: string; error: string };
+
   ngOnInit() {
     this.openDialog$ = this.recommendService.onOpenRecommend().pipe(map(() => this.open()));
+    this.form.valueChanges.subscribe(() => {
+      this.error = null;
+    });
   }
+
   open() {
     if (!this.dialogRef.nativeElement.open) {
       this.dialogRef.nativeElement.showModal();
     }
   }
+
   close() {
     this.dialogRef.nativeElement.close();
   }
+
   submit() {
+    console.log(this.form);
+    if (this.form.invalid) {
+      this.error = Object.entries(this.form.controls)
+        .map(([name, control]) => {
+          const error = control.errors ? Object.keys(control.errors)[0] : null;
+          return { name, error };
+        })
+        .find(r => r.error != null);
+
+      return;
+    }
     this.recommendService.recommendSubmit(this.recommend).subscribe(
       () => {
         this.dialogRef.nativeElement.close();
