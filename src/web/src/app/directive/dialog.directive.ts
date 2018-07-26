f622227aa4c81cd0ba86b3cf5b200ec417dc2171
myml <wurongjie1@gmail.com>
2018-07-26 10:24:01 +0800

    feat: donate statement add close button
    Change-Id: Iefff8c608f58a7a91fbc06342fb599cc0f635db0

--- src/web/src/app/directive/dialog.directive.ts
+++ src/web/src/app/directive/dialog.directive.ts
@@ -1,26 +1,27 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 import { JsonpClientBackend } from '@angular/common/http';
 
 @Directive({
   selector: 'dialog',
 })
 export class DialogDirective {
-  constructor(private elementRef: ElementRef<HTMLDialogElement>) {}
-  @HostListener('mousewheel', ['$event'])
-  mousewheel(e: MouseWheelEvent) {
-    e.stopPropagation();
-
-    const el = this.elementRef.nativeElement;
-    if (
-      (e.wheelDeltaY < 0 &&
-        el.scrollHeight - el.scrollTop <= el.clientHeight + loginDialogMargin) ||
-      (e.wheelDeltaY > 0 && el.scrollTop === 0)
-    ) {
-      e.preventDefault();
-    }
+  constructor(private elementRef: ElementRef<HTMLDialogElement>) {
+    const observer = new MutationObserver(() => {
+      if (this.elementRef.nativeElement.open) {
+        document.body.style.overflowY = 'hidden';
+      } else {
+        document.body.style.overflowY = 'auto';
+      }
+    });
+    observer.observe(this.elementRef.nativeElement, {
+      attributes: true,
+      attributeFilter: ['open'],
+    });
   }
-  @HostListener('click', ['$event'])
+
+  @HostListener('mousedown', ['$event'])
   click(e: Event) {
+    console.log(e);
     if (e.srcElement.nodeName === 'DIALOG' && this.elementRef.nativeElement.open) {
       this.elementRef.nativeElement.close();
     }
