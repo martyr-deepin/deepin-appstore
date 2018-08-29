160fa66057730713639e842727a1ccf553fcf03a
myml <wurongjie1@gmail.com>
2018-08-29 19:15:57 +0800

    feat: deepin info pipe batch getting
    Change-Id: Ie173370c9d9c31e9b545a970fc2da2bcad6566ef

--- src/web/src/app/dstore/pipes/deepin-info.pipe.ts
+++ src/web/src/app/dstore/pipes/deepin-info.pipe.ts
@@ -1,28 +1,49 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { memoize } from 'lodash';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'environments/environment';
+import { chunk } from 'lodash';
 
-import { BaseService } from '../services/base.service';
+import { Observable, Subject, forkJoin } from 'rxjs';
+import { map, bufferTime, filter, switchMap, first, share, tap } from 'rxjs/operators';
 
 @Pipe({
   name: 'deepinInfo',
 })
 export class DeepinInfoPipe implements PipeTransform {
-  constructor(private http: HttpClient) {}
-  transform = memoize(
-    (uid: number): Observable<DeepinInfo> => {
-      return this.http
-        .get<DeepinInfo[]>(BaseService.serverHosts.metadataServer + '/api/deepin_user', {
-          params: { uid: uid.toString() },
-        })
-        .pipe(
-          map(resp => resp.find(info => info.uid === uid)),
-          shareReplay(),
-        );
-    },
-  );
+  apiURL = environment.metadataServer + '/api/deepin_user';
+
+  buffer = new Subject<number>();
+  result: Observable<DeepinInfo[]>;
+
+  constructor(private http: HttpClient) {
+    // 搜集参数，统一查询
+    this.result = this.buffer.pipe(
+      bufferTime(10),
+      filter(list => list.length > 0),
+      switchMap(list => {
+        console.log(list);
+        return forkJoin(
+          chunk(list, 20)
+            .map(arr => {
+              let params = new HttpParams();
+              arr.forEach(uid => (params = params.append('uid', uid.toString())));
+              return params;
+            })
+            .map(params => this.http.get<DeepinInfo[]>(this.apiURL, { params })),
+        ).pipe(map(arrList => [].concat(...arrList)));
+      }),
+      share(),
+    );
+  }
+  transform(uid: number) {
+    console.log('uid', uid);
+    this.buffer.next(uid);
+    return this.result.pipe(
+      map(infoList => {
+        return infoList.find(info => info.uid === uid);
+      }),
+    );
+  }
 }
 
 export interface DeepinInfo {
