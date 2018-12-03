f035aea412ad105a102d5838f95aa699a1212b1e
myml <wurongjie1@gmail.com>
2018-12-03 16:03:18 +0800

    feat: add my donates components
    Change-Id: I1ed1f2c4a3e715809f487b5d8422557810d84c66

--- src/web/src/app/modules/my-donates/donates.service.ts
+++ src/web/src/app/modules/my-donates/donates.service.ts
@@ -1,9 +1,55 @@
+import { App } from './../../services/app.service';
+import { AppService } from 'app/services/app.service';
+import { switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DonatesService {
+  apiURL = environment.operationServer + '/api/my/donate';
+  constructor(private http: HttpClient, private appService: AppService) {}
+  donateList(page: number, count: number) {
+    return this.http
+      .get<Result>(this.apiURL, {
+        params: { page: page.toString(), count: count.toString() },
+      })
+      .pipe(
+        switchMap(
+          result => {
+            console.log('switchMap');
+            const names = result.donations.map(d => d.appName);
+            return this.appService.getApps(names, false, false);
+          },
+          (result, apps) => {
+            result.donations.forEach(d => {
+              d.app = apps.find(app => app.name === d.appName);
+            });
+            return result;
+          },
+        ),
+      );
+  }
+}
 
-  constructor() { }
+interface Result {
+  donations: Donation[];
+  totalCount: number;
+}
+
+interface Donation {
+  app: App;
+  id: number;
+  createTime: string;
+  donator: number;
+  appName: string;
+  amount: number;
+  currency: string;
+  exchange_rate: number;
+  actualAmount: number;
+  source: number;
+  tradeID: string;
+  appStore: string;
 }
--- src/web/src/app/modules/my-donates/donates/donates.component.ts
+++ src/web/src/app/modules/my-donates/donates/donates.component.ts
@@ -1,15 +1,50 @@
+import { Payment } from './../../details/services/donate.model';
+import { distinctUntilChanged, map, switchMap, share, tap } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DonatesService } from './../donates.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dstore-donates',
   templateUrl: './donates.component.html',
-  styleUrls: ['./donates.component.scss']
+  styleUrls: ['./donates.component.scss'],
 })
 export class DonatesComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private donatesService: DonatesService,
+  ) {}
+  Payment = Payment;
+  loaded = false;
+  // 监听列表高度
+  listHeight$ = new BehaviorSubject<number>(0);
+  // 根据列表高度计算列表行数
+  pageSize$ = this.listHeight$.pipe(
+    map(height => Math.floor(height / 160)),
+    distinctUntilChanged(),
+  );
+  // 监听当前页
+  pageIndex$ = this.route.queryParamMap.pipe(map(query => Number(query.get('page') || 1) - 1));
+  // 根据列表行数和页数的变动,拉取列表数据
+  result$ = combineLatest(this.pageIndex$, this.pageSize$).pipe(
+    switchMap(([pageIndex, pageSize]) => {
+      this.loaded = false;
+      return this.donatesService.donateList(pageIndex + 1, pageSize);
+    }),
+    share(),
+    tap(() => {
+      this.loaded = true;
+    }),
+  );
+  donates$ = this.result$.pipe(map(result => result.donations));
+  length$ = this.result$.pipe(map(result => result.totalCount));
 
   ngOnInit() {
+    this.result$.subscribe(console.log);
   }
-
+  gotoPage(pageIndex: number) {
+    this.router.navigate([], { queryParams: { page: pageIndex + 1 } });
+  }
 }
