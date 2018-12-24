8d8b9a88f22d4d9362f2e13f7a18a273557cc81a
Iceyer <me@iceyer.net>
2018-12-24 16:17:11 +0800

    misc: hide Privacy agreement
    Change-Id: Iadcb0602446daf8073a9260bc57cfb658781a38a

--- src/ui/widgets/title_bar_menu.h
+++ src/ui/widgets/title_bar_menu.h
@@ -51,7 +51,8 @@ class TitleBarMenu : public QMenu {
   QAction* region_international_ = nullptr;
   QString theme_name_ = "light";
   QAction* switch_theme_action_ = nullptr;
-  QAction* privacy_agreement_action_ = nullptr;
+  // TODO: just hide it now
+  //QAction* privacy_agreement_action_ = nullptr;
 
  private slots:
   void onThemeActionTriggered();
