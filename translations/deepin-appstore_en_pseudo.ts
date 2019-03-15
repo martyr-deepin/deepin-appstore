33daa4677bb2881be531223b16181c20712c8f8a
electricface <songwentai@deepin.com>
2019-03-15 16:40:16 +0800

    chore: auto pull translation files from transifex

---
+++ translations/deepin-appstore_en_pseudo.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="115"/>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="164"/>
+        <source>Search &quot;%1&quot; in Deepin Store</source>
+        <translation>[Şḗȧřƈħ &quot;%1&quot; īƞ Ḓḗḗƥīƞ Şŧǿřḗ ςǲ鶱ΰΰϵ ϑſǲ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar.cpp" line="201"/>
+        <source>Search</source>
+        <translation>[Şḗȧřƈħ ıϵ靐ϑ靐衋]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="103"/>
+        <source>Dark Theme</source>
+        <translation>[Ḓȧřķ Ŧħḗḿḗ ϑΐıς ǅıϑǋı]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="68"/>
+        <source>Recommend App</source>
+        <translation>[Řḗƈǿḿḿḗƞḓ Ȧƥƥ ϱııǅϖϱς鶱ſ ϱ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="74"/>
+        <source>Select Region</source>
+        <translation>[Şḗŀḗƈŧ Řḗɠīǿƞ 靐ϕ靐ΐϑẛ ϑſ衋衋]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="80"/>
+        <source>China</source>
+        <translation>[Ƈħīƞȧ ϐϐςϰϐ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="82"/>
+        <source>International</source>
+        <translation>[Īƞŧḗřƞȧŧīǿƞȧŀ ΐẛϖϑǲ鶱ıϰϖΐſ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="99"/>
+        <source>Clear Cache</source>
+        <translation>[Ƈŀḗȧř Ƈȧƈħḗ ϖΰϵ靐ϕ ǲςẛǲϱ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="73"/>
+        <source>Undo</source>
+        <translation>[Ŭƞḓǿ ǲϖ鶱ϕ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="78"/>
+        <source>Redo</source>
+        <translation>[Řḗḓǿ 靐ϵϵϖ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="84"/>
+        <source>Cut</source>
+        <translation>[Ƈŭŧ ǋǈΰ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="89"/>
+        <location filename="../src/ui/web_event_delegate.cpp" line="113"/>
+        <source>Copy</source>
+        <translation>[Ƈǿƥẏ 靐ſΐϖ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="94"/>
+        <source>Paste</source>
+        <translation>[Ƥȧşŧḗ ϖϐẛϕǲ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="99"/>
+        <source>Delete</source>
+        <translation>[Ḓḗŀḗŧḗ ϖſǲϱΰϰ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="105"/>
+        <source>Select all</source>
+        <translation>[Şḗŀḗƈŧ ȧŀŀ ΐıΐǈǲı ϖϕΐ]</translation>
+    </message>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="106"/>
+        <source>Deepin Store</source>
+        <translation>[Ḓḗḗƥīƞ Şŧǿřḗ 靐ϰϱΐϰΰ ıǲϖ]</translation>
+    </message>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="107"/>
+        <source>Deepin Store is an Appstore with quality and rich applications. Popular recommendation, new updated apps and hot topics are available. It supports one click to download, install, update, uninstall and so on.</source>
+        <translation>[Ḓḗḗƥīƞ Şŧǿřḗ īş ȧƞ Ȧƥƥşŧǿřḗ ẇīŧħ ɋŭȧŀīŧẏ ȧƞḓ řīƈħ ȧƥƥŀīƈȧŧīǿƞş. Ƥǿƥŭŀȧř řḗƈǿḿḿḗƞḓȧŧīǿƞ, ƞḗẇ ŭƥḓȧŧḗḓ ȧƥƥş ȧƞḓ ħǿŧ ŧǿƥīƈş ȧřḗ ȧṽȧīŀȧƀŀḗ. Īŧ şŭƥƥǿřŧş ǿƞḗ ƈŀīƈķ ŧǿ ḓǿẇƞŀǿȧḓ, īƞşŧȧŀŀ, ŭƥḓȧŧḗ, ŭƞīƞşŧȧŀŀ ȧƞḓ şǿ ǿƞ. ϑςϖǅϐϱ ı鶱ϑϖ鶱 ϰ靐 ϕǲ ǈ衋ΰ鶱ϐϐϑ鶱 ΰϑẛς ΐϕſϐϑ]</translation>
+    </message>
+</context>
+<context>
+    <name>dstore::UserMenu</name>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="13"/>
+        <source>Username</source>
+        <translation>[Ŭşḗřƞȧḿḗ ϰ衋ς鶱ϵǅϱ鶱]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="16"/>
+        <source>My comments</source>
+        <translation>[Ḿẏ ƈǿḿḿḗƞŧş 鶱鶱 ǋǲϕϑϕϖϰǋ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="20"/>
+        <source>My donations</source>
+        <translation>[Ḿẏ ḓǿƞȧŧīǿƞş ϕς ıǲςıϑϖϑ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="24"/>
+        <source>My apps</source>
+        <translation>[Ḿẏ ȧƥƥş ıϰ ΰϱϑϱ]</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="30"/>
+        <source>Sign out</source>
+        <translation>[Şīɠƞ ǿŭŧ ıΐ衋ς 衋ςſ]</translation>
+    </message>
+</context>
+</TS>
