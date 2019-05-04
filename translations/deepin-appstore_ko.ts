95f5d17b18cc54070d13cb4b6ad06f155bd2408d
MarongHappy <36289819+MarongHappy@users.noreply.github.com>
2019-05-05 01:49:09 +0900

    Create deepin-appstore_ko.ts

---
+++ translations/deepin-appstore_ko.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR" sourcelanguage="en_US">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="107"/>
+        <source>Deepin Store</source>
+        <translation type="unfinished">Deepin 스토어</translation>
+    </message>
+    <message>
+        <location filename="../src/app/deepin_appstore.cpp" line="108"/>
+        <source>Deepin Store is an Appstore with quality and rich applications. Popular recommendation, new updated apps and hot topics are available. It supports one click to download, install, update, uninstall and so on.</source>
+        <translation type="unfinished">Deepin 스토어는 품질과 풍부한 애플리케이션을 갖춘 앱스토어입니다. 일반적인 권장 사항, 새로운 업데이트된 앱 및 핫 항목을 사용할 수 있습니다. 한 번의 클릭으로 다운로드, 설치, 업데이트, 제거 등을 수행할 수 있습니다.</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="73"/>
+        <source>Undo</source>
+        <translation type="unfinished">되돌리기</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="78"/>
+        <source>Redo</source>
+        <translation type="unfinished">재실행</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="84"/>
+        <source>Cut</source>
+        <translation type="unfinished">자르기</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="89"/>
+        <location filename="../src/ui/web_event_delegate.cpp" line="113"/>
+        <source>Copy</source>
+        <translation type="unfinished">복사하기</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="94"/>
+        <source>Paste</source>
+        <translation type="unfinished">붙여넣기</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="99"/>
+        <source>Delete</source>
+        <translation type="unfinished">삭제하기</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/web_event_delegate.cpp" line="105"/>
+        <source>Select all</source>
+        <translation type="unfinished">모두 선택</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="115"/>
+        <location filename="../src/ui/widgets/search_completion_window.cpp" line="164"/>
+        <source>Search &quot;%1&quot; in Deepin Store</source>
+        <translation type="unfinished">Deepin 스토어에서 &quot;%1&quot; 검색</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar.cpp" line="201"/>
+        <source>Search</source>
+        <translation type="unfinished">검색</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="68"/>
+        <source>Recommend App</source>
+        <translation type="unfinished">추천 앱</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="74"/>
+        <source>Select Region</source>
+        <translation type="unfinished">지역 선택</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="80"/>
+        <source>China</source>
+        <translation type="unfinished">중국</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="82"/>
+        <source>International</source>
+        <translation type="unfinished">국가별</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="99"/>
+        <source>Clear Cache</source>
+        <translation type="unfinished">캐시 지우기</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="103"/>
+        <source>Dark Theme</source>
+        <translation type="unfinished">어두운 테마</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/title_bar_menu.cpp" line="109"/>
+        <source>Privacy agreement</source>
+        <translation type="unfinished">개인정보 보호 계약</translation>
+    </message>
+</context>
+<context>
+    <name>dstore::UserMenu</name>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="13"/>
+        <source>Username</source>
+        <translation type="unfinished">사용자 이름</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="16"/>
+        <source>My comments</source>
+        <translation type="unfinished">내 의견</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="20"/>
+        <source>My donations</source>
+        <translation type="unfinished">나의 기부</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="24"/>
+        <source>My apps</source>
+        <translation type="unfinished">내 앱</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/widgets/user_menu.cpp" line="30"/>
+        <source>Sign out</source>
+        <translation type="unfinished">로그 아웃</translation>
+    </message>
+</context>
+</TS>
