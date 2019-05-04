2887ae0c7810306f714320770908f47e235cf9ad
MarongHappy <36289819+MarongHappy@users.noreply.github.com>
2019-05-05 01:56:36 +0900

    desktop_ko.ts

---
+++ translations/desktop/desktop_ko.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR" sourcelanguage="en_US">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Comment" line="0"/>
+        <source>A general Linux software manager</source>
+        <translation type="unfinished">일반적인 리눅스 소프트웨어 관리자</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>App Store</source>
+        <translation type="unfinished">앱 스토어</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Deepin App Store</source>
+        <translation type="unfinished">Deepin 앱 스토어</translation>
+    </message>
+</context>
+</TS>
