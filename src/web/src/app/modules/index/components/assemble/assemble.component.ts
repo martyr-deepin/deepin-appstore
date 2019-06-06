b0f7fa3f8eeddc41aa5db0658095df164c1b7b56
myml <wurongjie1@gmail.com>
2019-06-06 17:03:18 +0800

    feat: rewrite index section item

---
+++ src/web/src/app/modules/index/components/assemble/assemble.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { SectionItemBase } from '../section-item-base';
+import { SectionAssemble } from 'app/dstore/services/section';
+import { Software, SoftwareService } from 'app/services/software.service';
+
+@Component({
+  selector: 'index-assemble',
+  templateUrl: './assemble.component.html',
+  styleUrls: ['./assemble.component.scss'],
+})
+export class AssembleComponent extends SectionItemBase implements OnInit {
+  constructor(private softwareService: SoftwareService) {
+    super();
+  }
+  assembles: SectionAssemble[];
+  softs = new Map<string, Promise<Software[]>>();
+  ngOnInit() {
+    this.assembles = (this.section.items as SectionAssemble[]).filter(a => a.show);
+    this.assembles.forEach(assemble => {
+      const softs$ = this.softwareService.list({ names: assemble.apps.map(app => app.name) });
+      this.softs.set(assemble.category, softs$);
+    });
+    this.wait().finally(() => this.loaded.emit(true));
+  }
+  async wait() {
+    for (const p of this.softs.values()) {
+      await p;
+    }
+  }
+}
--- src/web/src/app/modules/index/index.module.ts
+++ src/web/src/app/modules/index/index.module.ts
@@ -10,11 +10,13 @@ import { MoreComponent } from './components/more/more.component';
 import { CoverComponent } from './components/cover/cover.component';
 import { RankingComponent } from './components/ranking/ranking.component';
 import { PhraseComponent } from './components/phrase/phrase.component';
+import { AssembleComponent } from './components/assemble/assemble.component';
+import { TitleComponent } from './components/title/title.component';
 
 const components = [IndexComponent, TopicComponent, MoreComponent];
 
 @NgModule({
-  declarations: [...components, CoverComponent, RankingComponent, PhraseComponent],
+  declarations: [...components, CoverComponent, RankingComponent, PhraseComponent, AssembleComponent, TopicComponent, TitleComponent],
   imports: [CommonModule, IndexRoutingModule, ShareModule],
 })
 export class IndexModule {}
