e011ca5e0b2ff0e2c3e2040af6b2133bc14a4e56
myml <wurongjie1@gmail.com>
2019-06-19 18:57:37 +0800

    fix: can not uninstall software

--- src/web/src/app/modules/my-apps/services/local-app.service.ts
+++ src/web/src/app/modules/my-apps/services/local-app.service.ts
@@ -3,7 +3,7 @@ import { switchMap, map } from 'rxjs/operators';
 import { chunk } from 'lodash';
 
 import { JobService } from 'app/services/job.service';
-import { StoreService } from 'app/modules/client/services/store.service';
+import { StoreService, Package } from 'app/modules/client/services/store.service';
 import { StoreJobType } from 'app/modules/client/models/store-job-info';
 import { environment } from 'environments/environment';
 import { SoftwareService, Software } from 'app/services/software.service';
@@ -25,10 +25,12 @@ export class LocalAppService {
       switchMap(async installed => {
         console.log(installed);
         let list = chunk(installed, pageSize)[pageIndex].map(pkg => {
+          const local_name = pkg.allLocalName[environment.locale] || pkg.allLocalName['en_US'] || pkg.packageName;
           return {
             name: pkg.packageName,
             package: pkg,
-            local_name: pkg.allLocalName[environment.locale] || pkg.allLocalName['en_US'] || pkg.packageName,
+            localName: local_name,
+            info: { name: local_name, packages: [{ packageURI: pkg.packageURI }] },
             software: null as Software,
           };
         });
