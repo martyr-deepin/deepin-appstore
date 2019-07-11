786c66f08815f426519a3fefed55b23fcb5bfce0
myml <wurongjie1@gmail.com>
2019-07-11 13:21:46 +0800

    feat: ip region default 'CN'

--- src/web/src/app/services/region.service.ts
+++ src/web/src/app/services/region.service.ts
@@ -3,16 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { map } from 'rxjs/operators';
 
+import { get } from 'lodash';
+
 @Injectable({
   providedIn: 'root',
 })
 export class RegionService {
   constructor(private http: HttpClient) {}
   region() {
-    console.log('region');
     return this.http
       .get<Region>(environment.metadataServer + '/api/v3/region')
-      .pipe(map(region => region.Country.IsoCode));
+      .pipe(map(region => get(region, 'Country.IsoCode', 'CN')));
   }
 }
 interface Region {
